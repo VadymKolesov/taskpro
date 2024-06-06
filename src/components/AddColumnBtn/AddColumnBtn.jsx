1c024405c88e7131e32abfade4e06f07bd6d5c2a
Vadym <46047631+VadymKolesov@users.noreply.github.com>
2024-06-06 21:15:45 +0200

    edits (AddColumnBtn, AddColumnPopUp)

--- src/components/AddColumnBtn/AddColumnBtn.jsx
+++ src/components/AddColumnBtn/AddColumnBtn.jsx
@@ -3,10 +3,18 @@ import clsx from "clsx";
 import sprite from "../../assets/sprite.svg";
 
 export default function AddColumnBtn() {
-  const theme = "violet";
+  const theme = "dark";
+
+  const handleClick = () => {
+    console.log("hello");
+  };
 
   return (
-    <button className={clsx(css.btn, css[theme])}>
+    <button
+      className={clsx(css.btn, css[theme])}
+      type="button"
+      onClick={handleClick}
+    >
       <div className={clsx(css.plusCont, css[theme])}>
         <svg className={clsx(css.plusIcon, css[theme])}>
           <use href={`${sprite}#icon-plus`}></use>
--- src/components/AddColumnPopUp/AddColumnPopUp.jsx
+++ src/components/AddColumnPopUp/AddColumnPopUp.jsx
@@ -1,31 +1,59 @@
 import css from "./AddColumnPopUp.module.css";
 import clsx from "clsx";
-import { Formik, Form, Field } from "formik";
+import { Formik, Form, Field, ErrorMessage } from "formik";
 import Button from "../Button/Button";
 import sprite from "../../assets/sprite.svg";
+import * as Yup from "yup";
 
-export default function AddColumnPopUp({ titleValue, inputValue }) {
-  const theme = "violet";
-  const titleText = `${titleValue} column`;
+export default function AddColumnPopUp({ isEdit, inputValue }) {
+  const theme = "dark";
+
+  const schema = Yup.object({
+    name: Yup.string()
+      .required("Title is required")
+      .not([inputValue], "This title already use"),
+  });
+
+  const handleSubmit = (values, actions) => {
+    isEdit
+      ? console.log(`Edit ${values.name}`)
+      : console.log(`Add ${values.name}`);
+    actions.resetForm();
+  };
+
+  const handleClose = () => {
+    console.log("close");
+  };
 
   return (
     <div className={clsx(css.cont, css[theme])}>
-      <button className={clsx(css.closeBtn, css[theme])}>
+      <button className={clsx(css.closeBtn, css[theme])} onClick={handleClose}>
         <svg className={clsx(css.closeIcon, css[theme])}>
           <use href={`${sprite}#icon-x-close`}></use>
         </svg>
       </button>
-      <p className={clsx(css.text, css[theme])}>{titleText}</p>
-      <Formik initialValues={{ name: `${inputValue}` }}>
+      <p className={clsx(css.text, css[theme])}>
+        {isEdit ? "Edit column" : "Add column"}
+      </p>
+      <Formik
+        initialValues={{ name: inputValue }}
+        validationSchema={schema}
+        onSubmit={handleSubmit}
+      >
         <Form className={css.form}>
           <Field
             type="text"
-            name="title"
+            name="name"
             className={clsx(css.input, css[theme])}
             placeholder="Title"
           />
+          <ErrorMessage
+            name="name"
+            component="p"
+            className={clsx(css.error, css[theme])}
+          />
           <Button
-            text={titleValue}
+            text={isEdit ? "Edit" : "Add"}
             isIcon={true}
             verticalPadding="10px"
             type="submit"
