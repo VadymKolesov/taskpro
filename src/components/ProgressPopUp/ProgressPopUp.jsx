20a75275716dae5af5189f3c6e89e1a9e0339866
alexsharm <alfex.sharm@gmail.com>
2024-06-08 22:45:40 +0300

    array of objects processing

--- src/components/ProgressPopUp/ProgressPopUp.jsx
+++ src/components/ProgressPopUp/ProgressPopUp.jsx
@@ -2,8 +2,8 @@ import css from "./ProgressPopUp.module.css";
 import clsx from "clsx";
 import sprite from "../../assets/sprite.svg";
 
-export default function AddColumnBtn() {
-  const theme = "violet";
+export default function AddColumnBtn({ columns }) {
+  const theme = "dark";
 
   const handleClick = () => {
     console.log("hello");
@@ -11,26 +11,19 @@ export default function AddColumnBtn() {
 
   return (
     <div className={clsx(css.cont, css[theme])}>
-      <button
-        type="button"
-        className={clsx(css.btn, css[theme])}
-        onClick={handleClick}
-      >
-        <p>In progress</p>
-        <svg className={clsx(css.icon, css[theme])}>
-          <use href={`${sprite}#icon-move`}></use>
-        </svg>
-      </button>
-      <button
-        type="button"
-        className={clsx(css.btn, css[theme])}
-        onClick={handleClick}
-      >
-        <p>Done</p>
-        <svg className={clsx(css.icon, css[theme])}>
-          <use href={`${sprite}#icon-move`}></use>
-        </svg>
-      </button>
+      {columns.map((column) => (
+        <button
+          type="button"
+          className={clsx(css.btn, css[theme])}
+          onClick={handleClick}
+          key={column.id}
+        >
+          <p>{column.name}</p>
+          <svg className={clsx(css.icon, css[theme])}>
+            <use href={`${sprite}#icon-move`}></use>
+          </svg>
+        </button>
+      ))}
     </div>
   );
 }
