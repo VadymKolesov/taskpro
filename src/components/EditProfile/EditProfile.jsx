5ddef73aaad56a3f207682c1119dd15e44b08b27
Vadym <46047631+VadymKolesov@users.noreply.github.com>
2024-06-10 21:01:09 +0200

    edits

--- src/components/EditProfile/EditProfile.jsx
+++ src/components/EditProfile/EditProfile.jsx
@@ -1,6 +1,5 @@
 import { useState } from "react";
 import sprite from "../../assets/sprite.svg";
-// import ImageUploader from "./ImageUploader";
 import { ErrorMessage, Field, Form, Formik } from "formik";
 import clsx from "clsx";
 import * as Yup from "yup";
@@ -9,8 +8,11 @@ import Button from "../Button/Button";
 
 export default function EditProfile() {
   const [showPassword, setShowPassword] = useState(false);
+  const [userAvatar, setUserAvatar] = useState(
+    "https://png.pngtree.com/png-clipart/20190614/original/pngtree-sunny-little-boy-children-cute-little-boy-avatar-brown-big-eyes-png-image_3793761.jpg"
+  );
 
-  const theme = "light";
+  const theme = "dark";
 
   const initialValues = {
     name: "",
@@ -19,9 +21,11 @@ export default function EditProfile() {
   };
 
   const validationSchema = Yup.object({
-    name: Yup.string().min(2, "Too short"),
-    email: Yup.string().email("Invalid format").required(),
-    password: Yup.string().min(6),
+    name: Yup.string().min(2, "Name is too short").required("Name is required"),
+    email: Yup.string().email("Invalid format").required("Email is required"),
+    password: Yup.string()
+      .min(8, "Password is too short")
+      .required("Password is required"),
   });
 
   const handleSubmit = (values, actions) => {
@@ -29,16 +33,56 @@ export default function EditProfile() {
     actions.resetForm();
   };
 
+  const handleClose = () => {
+    console.log("close");
+  };
+
+  const handleImageChange = (event) => {
+    const file = event.target.files[0];
+
+    if (file) {
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        cropImage(reader.result, file);
+      };
+      reader.readAsDataURL(file);
+    }
+  };
+
+  const cropImage = (src, originalFile) => {
+    const img = new Image();
+    img.src = src;
+    img.onload = () => {
+      const canvas = document.createElement("canvas");
+      const ctx = canvas.getContext("2d");
+
+      const width = 68;
+      const height = 68;
+
+      canvas.width = img.width;
+      canvas.height = img.height;
+
+      ctx.drawImage(img, 0, 0);
+
+      const croppedImageUrl = canvas.toDataURL(originalFile.type);
+      setUserAvatar(croppedImageUrl);
+
+      // canvas.toBlob((blob) => {
+      //   const croppedFile = new File([blob], originalFile.name, {
+      //     type: originalFile.type,
+      //   });
+      //   dispatch(uploadAvatar(croppedFile));
+      // }, originalFile.type);
+    };
+  };
+
   const toggleShowPassword = () => {
     setShowPassword(!showPassword);
   };
 
-  const temporaryImage =
-    "https://png.pngtree.com/png-clipart/20190614/original/pngtree-sunny-little-boy-children-cute-little-boy-avatar-brown-big-eyes-png-image_3793761.jpg";
-
   return (
     <div className={clsx(css.EPContainer, css[theme])}>
-      <button className={css.closeBtn} type="button">
+      <button className={css.closeBtn} type="button" onClick={handleClose}>
         <svg
           className={clsx(css.closeIcon, css[theme])}
           width={"18"}
@@ -49,14 +93,21 @@ export default function EditProfile() {
       </button>
       <h2 className={css.title}>Edit profile</h2>
       <div className={css.profilePicture}>
-        <img
-          src={temporaryImage}
-          alt="Profile"
-          className={css.profileImage}
+        <input
+          id="image-input"
+          type="file"
+          accept="image/*"
+          className={css.input}
+          onChange={handleImageChange}
         />
-        <button type="button" className={clsx(css.uploadButton, css[theme])}>
-          +
-        </button>
+        <label htmlFor="image-input" className={css.inputLabel}>
+          <img src={userAvatar} alt="Profile" className={css.profileImage} />
+          <div className={clsx(css.uploadButton, css[theme])}>
+            <svg className={clsx(css.icon, css[theme])}>
+              <use href={`${sprite}#icon-plus`}></use>
+            </svg>
+          </div>
+        </label>
       </div>
       <Formik
         initialValues={initialValues}
@@ -73,7 +124,11 @@ export default function EditProfile() {
                   className={clsx(css.inputField, css[theme])}
                   placeholder="Name"
                 />
-                <ErrorMessage name="name" component="p" className={clsx(css.error, css[theme])} />
+                <ErrorMessage
+                  name="name"
+                  component="p"
+                  className={clsx(css.error, css[theme])}
+                />
               </li>
               <li className={css.inputGroup}>
                 <Field
