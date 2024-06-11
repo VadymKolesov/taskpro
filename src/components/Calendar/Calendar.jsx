b9bfa287c1d9caef7f1046c09f49b506e476dd0e
Ihor Chepliaka <ihor.chepliaka@gmail.com>
2024-06-11 17:49:58 +0300

    try to animate calendar

--- src/components/Calendar/Calendar.jsx
+++ src/components/Calendar/Calendar.jsx
@@ -1,17 +1,21 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import ForwardedCustomInput from "./CustomInput";
 import clsx from "clsx";
 import "./Calendar.css";
+import { CSSTransition } from "react-transition-group";
 
 export default function Calendar() {
   const theme = "dark";
   const [selectedDate, setSelectedDate] = useState(new Date());
+  const [isCalendarOpen, setIsCalendarOpen] = useState(false);
+
+  const calendarRef = useRef(null);
 
   const handleSetDate = (date) => {
     setSelectedDate(date);
-    console.log(selectedDate.getTime());
+    setIsCalendarOpen(false);
   };
 
   const datepickerWrapper = clsx(
@@ -19,14 +23,29 @@ export default function Calendar() {
     theme && `${theme}`
   );
 
+  const handleInputClick = () => {
+    setIsCalendarOpen(!isCalendarOpen);
+  };
+
   return (
     <div className={datepickerWrapper}>
-      <DatePicker
-        selected={selectedDate}
-        onChange={(date) => handleSetDate(date)}
-        dateFormat="eeee, MMMM d"
-        customInput={<ForwardedCustomInput />}
-      />
+      <ForwardedCustomInput onClick={handleInputClick} value={selectedDate} />
+      <CSSTransition
+        in={isCalendarOpen}
+        timeout={300}
+        classNames="calendar"
+        unmountOnExit
+        nodeRef={calendarRef}
+      >
+        <div className="calendar-wrapper" ref={calendarRef}>
+          <DatePicker
+            selected={selectedDate}
+            onChange={(date) => handleSetDate(date)}
+            dateFormat="eeee, MMMM d"
+            inline
+          />
+        </div>
+      </CSSTransition>
     </div>
   );
 }
--- src/components/Calendar/CustomInput.jsx
+++ src/components/Calendar/CustomInput.jsx
@@ -3,6 +3,30 @@ import css from "./Calendar.module.css";
 import clsx from "clsx";
 import sprite from "../../assets/sprite.svg";
 
+function formatDate(value) {
+  if (value instanceof Date) {
+    const today = new Date();
+    const datePart = value.toLocaleDateString("en-US", {
+      month: "long",
+      day: "numeric",
+    });
+
+    if (
+      value.getFullYear() === today.getFullYear() &&
+      value.getMonth() === today.getMonth() &&
+      value.getDate() === today.getDate()
+    ) {
+      return `Today, ${datePart}`;
+    }
+    return value.toLocaleDateString("en-US", {
+      weekday: "long",
+      month: "long",
+      day: "numeric",
+    });
+  }
+  return value;
+}
+
 function CustomInput({ value, onClick }, ref) {
   const theme = "dark";
   return (
@@ -11,7 +35,7 @@ function CustomInput({ value, onClick }, ref) {
       onClick={onClick}
       ref={ref}
     >
-      {value}
+      {formatDate(value)}
       <svg className={clsx(css.icon, css[theme])}>
         <use href={`${sprite}#icon-arrow-down`}></use>
       </svg>
