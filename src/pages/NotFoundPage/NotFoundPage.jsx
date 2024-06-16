621838fc837c35ebcfba1ac4fb49306972d99c14
Myroslava <mir_ok@ukr.net>
2024-06-16 23:39:11 +0300

    PageNotFound

--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -1,3 +1,33 @@
+import { useNavigate } from "react-router-dom";
+import cactus_icon from "../../assets/cactus_nf.png";
+import Button from "../../components/Button/Button";
+import { useSelector } from "react-redux";
+import { selectTheme } from "../../redux/auth/selectors";
+import clsx from "clsx";
+import css from "./NotFoundPage.module.css";
+
 export default function NotFoundPage() {
-  return <h1>Not found page</h1>;
+  const navigate = useNavigate();
+  const theme = useSelector(selectTheme);
+
+  const handleHomeClick = () => {
+    navigate("/home");
+  };
+
+  return (
+    <div className={clsx(css.page, css[theme])}>
+      <img className={css.nf_img} alt="Page not found" src={cactus_icon} />
+      <p className={clsx(css.header, css[theme])}>Whoops!</p>
+      <p className={clsx(css.text, css[theme])}>Page not found</p>
+
+      <Button
+        className={css.btn}
+        text="Home"
+        isIcon={false}
+        verticalPadding="10px"
+        type="button"
+        onClick={handleHomeClick}
+      />
+    </div>
+  );
 }
