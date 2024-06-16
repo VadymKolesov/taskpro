26f29f2b29f3dca60b3e279833766dab3398d647
Vadym <46047631+VadymKolesov@users.noreply.github.com>
2024-06-16 23:10:26 +0200

    compelete for cards

--- src/components/SideBar/BoardsList/BoardsList.jsx
+++ src/components/SideBar/BoardsList/BoardsList.jsx
@@ -1,22 +1,11 @@
 import css from "./BoardsList.module.css";
 import BoardItem from "../BoardItem/BoardItem";
-import { NavLink, useNavigate } from "react-router-dom";
-import { nanoid } from "nanoid";
-import clsx from "clsx";
-import sprite from "../../../assets/sprite.svg";
 import { useDispatch, useSelector } from "react-redux";
-import {
-  setBoardModalOpen,
-  setIsBoardEdit,
-  setIsConfirmBoardDelete,
-  setIsConfirmDeleteOpen,
-  setSideBarOpen,
-} from "../../../redux/controls/slice";
-import { selectBoards, selectTheme } from "../../../redux/auth/selectors";
+import { setSideBarOpen } from "../../../redux/controls/slice";
+import { selectBoards } from "../../../redux/auth/selectors";
 import { motion } from "framer-motion";
 
 export default function BoardsList() {
-  const theme = useSelector(selectTheme);
   const list = useSelector(selectBoards);
 
   const dispatch = useDispatch();
@@ -25,76 +14,20 @@ export default function BoardsList() {
     dispatch(setSideBarOpen(false));
   };
 
-  const handleUpdate = () => {
-    setTimeout(() => {
-      dispatch(setIsBoardEdit(true));
-      dispatch(setBoardModalOpen(true));
-    }, 200);
-  };
-
-  const handleDelete = () => {
-    setTimeout(() => {
-      dispatch(setIsConfirmBoardDelete(true));
-      dispatch(setIsConfirmDeleteOpen(true));
-    }, 200);
-  };
-
   return (
     <ul className={css.list}>
-      {Array.isArray(list) &&
-        list.map((item) => {
-          return (
-            <motion.li
-              whileTap={{ scale: 0.97 }}
-              key={nanoid()}
-              className={css.item}
-            >
-              <NavLink
-                to={`${item._id}`}
-                className={({ isActive }) =>
-                  clsx(css.link, isActive && css.isActive, css[theme])
-                }
-                onClick={handleSidebarClose}
-              >
-                {({ isActive }) => (
-                  <>
-                    <BoardItem board={item} />
-
-                    {isActive && (
-                      <div className={css.controls}>
-                        <ul className={css.controlsList}>
-                          <li>
-                            <button
-                              type="button"
-                              onClick={() => handleUpdate(item._id)}
-                              className={css.itemBtn}
-                            >
-                              <svg className={clsx(css.btnIcon, css[theme])}>
-                                <use href={`${sprite}#icon-pencil`}></use>
-                              </svg>
-                            </button>
-                          </li>
-                          <li>
-                            <button
-                              type="button"
-                              onClick={handleDelete}
-                              className={css.itemBtn}
-                            >
-                              <svg className={clsx(css.btnIcon, css[theme])}>
-                                <use href={`${sprite}#icon-trash`}></use>
-                              </svg>
-                            </button>
-                          </li>
-                        </ul>
-                        <div className={clsx(css.border, css[theme])}></div>
-                      </div>
-                    )}
-                  </>
-                )}
-              </NavLink>
-            </motion.li>
-          );
-        })}
+      {list.map((item) => {
+        return (
+          <motion.li
+            whileTap={{ scale: 0.97 }}
+            key={item._id}
+            className={css.item}
+            onClick={handleSidebarClose}
+          >
+            <BoardItem board={item} />
+          </motion.li>
+        );
+      })}
     </ul>
   );
 }
